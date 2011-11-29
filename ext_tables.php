19704725297b7b96629409116e2bd296f27b560c
root <root@virtual-4.mklv.de>
2011-11-29 01:32:02 +0100

    TASK: initial commit

---
+++ ext_tables.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+// Include static template for PhotoBlog Theme
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', '[yag] Theme PhotoBlog');
+
+?>
